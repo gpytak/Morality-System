7e96a619fb9ae8461e0f808f140a699e03bb2d96
Gregory Pytak <gpytak@gmail.com>
2023-10-13 20:24:55 -0700

    Updated Player, Camera, and Background
    The player's physics have been updated as well as a camera which follows the player with the background following as well.

---
+++ Assets/Assets/Scripts/Player.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    
+
+    // public void OnMove(InputValue value)
+    // {
+    //     moveDir = value.Get<Vector2>();
+    // }
+    
+}
---
+++ Assets/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    // public void Move()
+    // {
+
+    // }
+
+    // public void Talk()
+    // {
+        
+    // }
+
+    // public void Interact()
+    // {
+        
+    // }
+
+    // public void Open_Game_Menu()
+    // {
+        
+    // }
+
+    // public void Accept_Quest()
+    // {
+        
+    // }
+
+    // public void Open_Quests()
+    // {
+        
+    // }
+
+    // public void Open_Inventory()
+    // {
+        
+    // }
+
+    // public void Open_Discovery_Log()
+    // {
+        
+    // }
+}
