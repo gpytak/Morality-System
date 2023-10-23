a85f5c07b4235f041f618337bdab60e3332b1b0e
Gregory Pytak <gpytak@gmail.com>
2023-10-22 19:22:50 -0700

    Added in DialogueCanvas and NPCDialoguei
    Added in the beginning of the DialogueCanvas frame for NPC dialogue and button options. Added in NPCDialogue script while prompts a speech bubble above NPCs and face them towards the player.

---
+++ Assets/Scripts/NPCDialogue.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+
+public class NPCDialogue : MonoBehaviour
+{
+    private Transform player; // Allows the NPC to follow the player's position, so he can turn toward the player
+    private SpriteRenderer speechBubbleRenderer; // Allows speech to turn on and off
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speechBubbleRenderer = GetComponent<SpriteRenderer>();
+        speechBubbleRenderer.enabled = false;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision) // Called as long as a Collider2D is detected within its trigger range
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            // Speech bubble on
+            speechBubbleRenderer.enabled = true;
+
+            // Find the player's Transform
+            player = collision.gameObject.GetComponent<Transform>();
+
+            // Check to see where the player is, and then turn to the player
+            if(player.position.x > transform.position.x && transform.parent.localScale.x < 0)
+            {
+                Flip();
+            } 
+            else if(player.position.x < transform.position.x && transform.parent.localScale.x > 0)
+            {
+                Flip();
+            }
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            // Speech bubble off
+            speechBubbleRenderer.enabled = false;
+        }
+    }
+
+    private void Flip() 
+    {
+        Vector3 currentScale = transform.parent.localScale;
+        currentScale.x *= -1;
+        transform.parent.localScale = currentScale;
+    }
+}
